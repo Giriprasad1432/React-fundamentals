bc9762701c1c60e7a71932d28d4dfe64d423c544
Giriprasad1432 <giribabu121989@gmail.com>
2025-09-13 12:31:16 +0530

    react fundamentals part2

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,14 +1,35 @@
 import Profile from "./Profile.jsx"
+import "./ProductCart.css"
+import "./App.css"
+import Giri from "./Giri.jsx"
 import ProductCart from "./ProductCart.jsx"
 function App() {
-    const product_name="drinks"
-    const price=40
-    const isavailable=true
+  const object1={
+      product_name:"drinks",
+     price:40,
+     isavailable:true
+  }
+    const object2={
+      product_name:"toys",
+     price:9999,
+     isavailable:false
+  }
+  const prem={
+    name:"PREM SAGAR",
+    course: "B.tech(CSE)",
+    blood_group:"o -VE",
+    roll_no:"24VV1A0518",
+    dob:"18/17/2028"
+  }
 
   return (
-    <div>
-      <Profile/>
-      <ProductCart product_name={product_name} price={price} isavailable={true}/>
+    <div class="box2">
+      {/* <Profile/>
+      <div class="box1">
+      <ProductCart {...object1}/>
+      <ProductCart {...object2}/>
+      </div> */}
+      <Giri {...prem}/>
     </div>
   )
 }
--- practice/src/ProductCart.jsx
+++ practice/src/ProductCart.jsx
@@ -1,12 +1,18 @@
-function ProductCart(Product){
+function ProductCart({product_name, price, isavailable}){
     
     return(
         <>
-        <p>Product name : {Product.product_name} , 
-            price: {Product.price} ,
-            Availability:  {Product.isavailable ===true? "drinks are available":"drinks are not available" }</p>
+    <div class="box">
+        <p class="name">Product name : {product_name}</p>
+
+         <p class="price">price : {price} </p>
+
+          <p class="stock">
+              {isavailable===true ? <p class="p1">drinks are available</p>:
+              <p class="p2">drinks are not available</p>}</p>
+        </div>
         </> 
 
     )
 }
-export default ProductCart;
+export default ProductCart
